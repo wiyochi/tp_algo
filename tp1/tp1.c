7fdacbfc562e5dd3e6af4deb8805ff53ba0b51bb
wiyochi <mat.arquilliere@gmail.com>
2019-02-27 17:17:31 +0100

    Ajout de tout les commentaires

--- tp1/tp1.c
+++ tp1/tp1.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include "LC.h"
 
+void    menuModifDate       (Message_t*);
+void    menuAfficheMotif    (Message_t*);
 int     lireFichier         (Message_t**, const char*);
 int     ecrireFichier       (Message_t*, const char*);
 void    afficherNonExpire   (Message_t*);
@@ -13,10 +15,28 @@ int     dateAj              ();
 void    formateChaine       (char*);
 void    afficherElement     (Message_t*);
 void    afficherListe       (Message_t*);
-void    menuModifDate       (Message_t*);
-void    menuAfficheMotif    (Message_t*);
 
 
+/*-------------------------------------------------------------------------
+ * main   Point d'entree du programme
+ *                                                                         
+ * Entrées: argc, entier representant le nombre d'arguments passes au programme
+ *          argv, tableau de chaine de caractere representants les arguments
+ *                  passes au programme (en l'occurence le nom du fichier d'entree
+ *                   et le nom du fichier de sortie)
+ *
+ *  Si on n'a pas de nom de fichier d'entree et de nom de fichier de sortie le
+ *      programme affiche simplement "Pas de fichier" et s'arrete.
+ *  Sinon on lis le fichier donne, on en creer une liste de message puis
+ *      on affiche les messages non expires, on les supprime de la liste et
+ *      on ecris dans le fichier de sortie la liste.
+ * Puis l'utilisateur peut choisir d'afficher la liste, de modifier la date,
+ *      d'afficher les messages avec un motif ou de quitter le programme.
+ *
+ * Lexique: maListe, pointeur de tete sur la liste chainnee de message
+ *          choix, entier representant le choix de l'utilisateur pour les menus
+ *-------------------------------------------------------------------------
+ */
 int main(int argc, char* argv[])
 {
     Message_t*  maListe = NULL;
@@ -24,7 +44,7 @@ int main(int argc, char* argv[])
 
     if(argc < 3)
     {
-        printf("pas de fichier\n");
+        printf("Pas de fichier\n");
     }
     else
     {
@@ -39,7 +59,6 @@ int main(int argc, char* argv[])
 
         printf("\n\nSupression des messages expires...\n");
         supprimerExpire(&maListe);
-        afficherListe(maListe);
 
         printf("\n\nEcriture dans le fichier %s...\n", argv[2]);
         if(ecrireFichier(maListe, argv[2]))
@@ -87,9 +106,12 @@ int main(int argc, char* argv[])
  *                                                                         
  * Entrées: liste, pointeur de la liste                                    
  *
- *  Permet 
+ *  On recupere de l'utilisateur la date de debut des messages qui seront a
+ *    modifies et la date de debut qui la remplacera puis on appelle la fonction
+ *    "modifDateDebut".
  *
- * Lexique: 
+ * Lexique: dateDebut, entier qui represente la date de debut a modifier
+ *          dateModif, entier qui represente la date modifiee
  *-------------------------------------------------------------------------
  */
 void menuModifDate(Message_t* liste)
@@ -106,6 +128,17 @@ void menuModifDate(Message_t* liste)
     afficherListe(liste);
 }
 
+/*-------------------------------------------------------------------------
+ * menuAfficheMotif   Permet à l'utilisateur d'afficher les messages selon un motif           
+ *                                                                         
+ * Entrées: liste, pointeur de la liste                                    
+ *
+ *  On recupere de l'utilisateur le motif, on la formate en chaine de caractere
+ *      (finissant par '\0') et on appelle la fonction "afficherMotif".
+ *
+ * Lexique: motif, un tableau de caracteres contenant le motif (max: 255 caracteres)
+ *-------------------------------------------------------------------------
+ */
 void menuAfficheMotif(Message_t* liste)
 {
     char motif[255];
@@ -121,14 +154,24 @@ void menuAfficheMotif(Message_t* liste)
  * lireFichier  Lit un fichier texte et stocke sont contenu dans une liste 
  *              de messages                                                
  *                                                                         
- * Entr�es: liste, adresse du pointeur de liste                            
- *          filename, cha�ne de caract�re contenant le nom du fichier      
+ * Entrees: liste, adresse du pointeur de liste                            
+ *          filename, chaine de caractere contenant le nom du fichier
+ * 
+ * Sortie: erreur, un entier que l'on renvoie a la fin de la foncion
+ *                  1 -> le fichier a bien ete ouvert
+ *                  0 -> le fichier n'a pas pu etre ouvert    
  *                                                                         
  *  Cette fonction lit le fichier filename ligne par ligne. En effet sur   
  *  une ligne du fichier se trouve dateDebut, dateFin et le message. Une   
- *  fois la ligne lue, on stocke son contenu dans une nouvel �l�ment de    
- *  type Message_t gr�ce � la fonction creerElement et que l'on ins�re     
- *  dans la liste en respectant le tri gr�ce � la proc�dure ajouterElement 
+ *  fois la ligne lue, on stocke son contenu dans un nouvel element de    
+ *  type Message_t grace a la fonction creerElement et que l'on insere     
+ *  dans la liste en respectant le tri grace a la procedure ajouterElement 
+ * 
+ * Lexique: file, un pointeur sur le fichier
+ *          ddebut, dfin, 2 entiers contenant les dates du message lu dans le fichier
+ *          msg, un tableau de caractere contenant la chaine de caractere du message
+ *                  lu dans le fichier (max: 100 caracteres)
+ *          element, pointeur du nouvel element cree a chaque message lu dans le fichier
  *-------------------------------------------------------------------------
  */
 int lireFichier(Message_t** liste, const char* filename)
@@ -167,11 +210,18 @@ int lireFichier(Message_t** liste, const char* filename)
 /*-------------------------------------------------------------------------
  *  ecrireFichier Ecrit le contenu d'une liste de messages dans un fichier 
  *                                                                         
- *  Entr�es: liste, pointeur de la liste                                   
- *           filename, cha�ne de caract�re contenant le nom du fichier     
+ *  Entrees: liste, pointeur de la liste                                   
+ *           filename, chaine de caractere contenant le nom du fichier     
+ * 
+ * Sortie: erreur, un entier que l'on renvoie a la fin de la foncion
+ *                  1 -> le fichier a bien ete ouvert
+ *                  0 -> le fichier n'a pas pu etre ouvert    
  *                                                                         
- *  Cette fonction va stocker chaque �l�ments de la liste dans le fichier  
- *  � raison d'une ligne par �l�ments                                      
+ *  Cette fonction va stocker chaque elements de la liste dans le fichier  
+ *  a raison d'une ligne par elements.               
+ * 
+ * Lexique: file, un pointeur sur le fichier
+ *          cour, un pointeur sur le message de la liste que l'on ecrit        
  *-------------------------------------------------------------------------
  */
 int ecrireFichier(Message_t* liste, const char* filename)
@@ -198,15 +248,16 @@ int ecrireFichier(Message_t* liste, const char* filename)
 }
 
 /*-------------------------------------------------------------------------
- *  afficherNonExpire   Affiche tous les messages dont leur dateFin est    
- *                      sup�rieur ou �gal � la date d'aujourd'hui          
+ *  afficherNonExpire   Affiche tous les messages non expires         
  *                                                                         
- *  Entr�es:    liste, pointeur de la liste                                
+ *  Entrees:    liste, pointeur de la liste                                
  *                                                                         
- *  Cette fonction va stocker dans une variable de type entier la date     
- *  d'aujourd'hui et va comparer cette derni�re avec dateFin de chaque     
- *  �l�ments. Si dateFin est sup�rieur ou �gal � la date d'aujourd'hui     
- *  alors on affiche le message de l'�l�ment.                              
+ *  Parcours les messages de la liste et n'affiche que ceux dont la date de
+ *      fin est superieur ou egal a la date d'aujourd'hui.
+ * 
+ * Lexique: cour, pointeur parcourant les messages de la liste
+ *          today, entier representant la date d'aujourd'hui acquise par
+ *                  la fonction "dateAj"       
  *-------------------------------------------------------------------------
  */
 void afficherNonExpire(Message_t* liste)
@@ -224,8 +275,22 @@ void afficherNonExpire(Message_t* liste)
     }
 }
 
-
-
+/*-------------------------------------------------------------------------
+ *  supprimerExpire   Supprime tous les messages dont leur dateFin est    
+ *                      inferieur a la date d'aujourd'hui          
+ *                                                                         
+ *  Entrees:    liste, adresse du pointeur de la liste                                
+ *                                                                         
+ *  Parcours les messages de la liste et supprime les messages dont la date 
+ *      de fin est inferieur a la date d'aujourd'hui grace a un precedent et
+ *      a la fonction "suppressionCellule".
+ * 
+ * Lexique: cour, pointeur parcourant les messages de la liste
+ *          prec, pointeur sur le champs suivant de l'élément précédent à cour
+ *          aj, entier representant la date d'aujourd'hui acquise par
+ *                  la fonction "dateAj"       
+ *-------------------------------------------------------------------------
+ */
 void supprimerExpire(Message_t** liste)
 {
     Message_t*  cour = *liste;
@@ -247,6 +312,20 @@ void supprimerExpire(Message_t** liste)
     }
 }
 
+/*-------------------------------------------------------------------------
+ *  modifDateDebut   Remplace des dates de debut de messages par une autre
+ *                                                                         
+ *  Entrees:    liste, pointeur de la liste
+ *              dateAModif, entier representant la date de debut a modifier
+ *              nDate, entier representant la nouvelle date a mettre dans
+ *                  les messages ayant dateAModif en date de debut
+ *                                                                         
+ *  Parcours la liste de message, teste si la date de debut correspond a
+ *      dateAModif et si c'est le cas la change avec nDate.
+ * 
+ * Lexique: cour, pointeur parcourant les messages de la liste       
+ *-------------------------------------------------------------------------
+ */
 void modifDateDebut(Message_t* liste, int dateAModif, int nDate)
 {
     Message_t* cour = liste;
@@ -256,11 +335,28 @@ void modifDateDebut(Message_t* liste, int dateAModif, int nDate)
         if(cour->dateDebut == dateAModif)
         {
             cour->dateDebut = nDate;
+            if(cour->dateFin < nDate)
+                cour->dateFin = nDate;
         }
         cour = cour->suivant;
     }
 }
 
+/*-------------------------------------------------------------------------
+ *  afficherMotif   affiche les messages de la liste ayant un texte avec un
+ *                      motif donne
+ *                                                                         
+ *  Entrees:    liste, pointeur de la liste
+ *              motif, tableau de caractere contenant le motif a rechercher
+ *                                                                         
+ *  Parcours la liste de message, teste si le texte du message contient le
+ *     motif grace a la fonction "rechercherMotif" et si c'est le cas l'affiche
+ * 
+ * Lexique: cour, pointeur parcourant les messages de la liste
+ *          avecMotif, pointeur sur le premier caractere du motif dans le
+ *                      message si il y en a un, NULL sinon.
+ *-------------------------------------------------------------------------
+ */
 void afficherMotif(Message_t* liste, const char* motif)
 {
     Message_t*  cour = liste;
@@ -278,11 +374,31 @@ void afficherMotif(Message_t* liste, const char* motif)
     }
 }
 
+/*-------------------------------------------------------------------------
+ *  rechercherMotif   recherche la premiere occurence d'un motif dans une
+ *                      chaine de caractere
+ *                                                                         
+ *  Entrees:    message, chaine de caractere dans laquelle on cherche un motif
+ *              motif, chaine de caractere contenant le motif a rechercher   
+ * 
+ * Sortie: debut, pointeur de caractere pointant sur le debut du motif trouve
+ *                  dans le message ou NULL si il n'y en a pas
+ *                                                                         
+ *  Parcours le message, si les caracteres sont en commun avec ceux du
+ *      motif on parcours egalement le motif. Si on arrive a la fin du
+ *      motif on s'arrete car on a trouve le motif. Si on trouve un caratere
+ *      different on continue le parcours du message et on recommence le 
+ *      parcours du motif. (Et cela jusqu'a la fin du message)
+ * 
+ * Lexique: cour, pointeur de caractere parcourant le message
+ *          courMotif, pointeur de caractere parcourant le motif
+ *-------------------------------------------------------------------------
+ */
 char* rechercherMotif(char* message, const char* motif)
 {
-    char* cour      = message;
+    char*       cour      = message;
     const char* courMotif = motif;
-    char* debut;
+    char*       debut;
 
     while(*cour != '\0' && *courMotif != '\0')
     {
@@ -305,6 +421,20 @@ char* rechercherMotif(char* message, const char* motif)
     return debut;
 }
 
+/*-------------------------------------------------------------------------
+ *  dateAj   renvoie la date d'aujourd'hui (date systeme)
+ * 
+ * Sortie: entier representant la date d'aujourd'hui sous la forme "aaaammjj"
+ *                                                                         
+ *  Utilise les structures et les fonctions systemes (time_t, struct tm,
+ *      time(), localtime() et strftime()) pour ecrire dans lun buffer la date
+ *      systeme et la renvoyer sous forme entiere.
+ * 
+ * Lexique: t, temps systeme
+ *          date, structure contenant la date du temps systeme
+ *          buffer, tableau de caractere contenant la date sous la forme "aaaammjj"
+ *-------------------------------------------------------------------------
+ */
 int dateAj()
 {
     time_t      t;
@@ -318,6 +448,19 @@ int dateAj()
     return atoi(buffer);
 }
 
+/*-------------------------------------------------------------------------
+ *  formateChaine   formate un tableau de caractere en chaine de caractere
+ * 
+ * Entrees: chaine, tableau de caractere finissant pas '\0' ou '\n'
+ *                                                                         
+ *  Parcours le tableau jusqu'au '\0' ou '\n' et le remplace par '\0'
+ *      Si la chaine etait correcte -> pas de modification
+ *      Sinon -> on la met au format chaine de caractere en C (tableau
+ *                      de caractere finissant par '\0')
+ * 
+ * Lexique: i, entier permettant de parcourir le tableau
+ *-------------------------------------------------------------------------
+ */
 void formateChaine(char* chaine)
 {
     int i = 0;
@@ -328,10 +471,13 @@ void formateChaine(char* chaine)
 }
 
 /*-------------------------------------------------------------------------
- * afficherListe   Affiche tout les �l�ments d'une liste de message        
+ * afficherListe   Affiche tout les elements d'une liste de message        
  *                                                                         
- * Entr�es: liste, pointeur de la liste                                    
- *                                                                         
+ * Entrees: liste, pointeur de la liste
+ * 
+ *  Parcours la liste et en affiche tous les elements
+ *  
+ * Lexique: cour, pointeur de message parcourant la liste                                                                       
  *-------------------------------------------------------------------------
  */
 void afficherListe(Message_t* liste)
@@ -346,10 +492,12 @@ void afficherListe(Message_t* liste)
 }
 
 /*-------------------------------------------------------------------------
- * afficherElement   Affiche un �l�ment d'une liste de message             
+ * afficherElement   Affiche un element
  *                                                                         
- * Entr�es: element, pointeur sur un structure message                     
- *                                                                         
+ * Entrees: element, pointeur sur un structure message
+ * 
+ * Affiche le contenu d'un element : sa date de debut, sa date de fin et le
+ *  texte associé.                                                                        
  *-------------------------------------------------------------------------
  */
 void afficherElement(Message_t* element)
