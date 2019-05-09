14d9b2168d165674dfd139fc0f8e853bc140f276
PetitMargoulin <bastou_23@hotmail.fr>
2019-05-09 12:03:51 +0200

    voila

---
+++ tp3/affichage.h
@@ -0,0 +1,15 @@
+#ifndef _AFFICHE_H_
+#define _AFFICHE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "arbre.h"
+#define TAILLE_MAX 20
+
+
+void affichagePile(Pile_t*);
+
+void affichage( noeud_t** );
+
+#endif // !_AFFICHAGE_H_
